f9a52fa1e9e199108b4f48219c0c7cc4259c8e73
Alisher <kuanyshbekovalisher57@gmail.com>
2023-10-22 20:59:51 +0600

    Add all actions of Stopwatch functionality

--- Neobis_iOS_StopWatch/ViewController.swift
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -17,13 +17,20 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
-    
     @IBOutlet weak var timePickerView: UIPickerView!
-    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     
     @IBOutlet weak var switchSegmentedControl: UISegmentedControl!
     
     var currentFunctionality = Functionality.Timer
+    
+    var timer: Timer!
+    
+    var startedTime: Date!
+    var timeOnRunning: DateComponents!
+    
+    var isTimerRunning = false
+    var isTimerPaused = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +39,48 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         timePickerView.dataSource = self
     }
 
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if !isTimerRunning {
+            startedTime = Date()
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTime), userInfo: nil, repeats: true)
+            isTimerRunning = true
+        }
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        if !isTimerPaused && isTimerRunning {
+            timeOnRunning = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: Date(), to: (startedTime))
+            
+            timer.invalidate()
+            isTimerPaused = true
+        } else if isTimerPaused {
+            startedTime = Calendar.current.date(byAdding: timeOnRunning, to: Date())
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTime), userInfo: nil, repeats: true)
+            isTimerPaused = false
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        isTimerRunning = false
+        isTimerPaused = false
+        timeOnRunning = nil
+        timerLabel.text = "00:00:00"
+    }
+    
+    
+    
+    @objc func UpdateTime() {
+        let userCalendar = Calendar.current
+        let currentTime = Date()
+        
+        // Change the seconds to days, hours, minutes and seconds
+        let timeLeft = userCalendar.dateComponents([.day, .hour, .minute, .second], from: (startedTime), to: currentTime)
+        
+        // Display Countdown
+        timerLabel.text = "\(String(format: "%02d",timeLeft.hour!)):\(String(format: "%02d",timeLeft.minute!)):\(String(format: "%02d",timeLeft.second!))"
+    }
+    
     @IBAction func switchFuntionality(_ sender: Any) {
         if (currentFunctionality == Functionality.Timer) {
             currentFunctionality = Functionality.StopWatch
@@ -63,7 +112,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         let minute = String(format: "%02d", Int(pickerView.selectedRow(inComponent: 1)))
         let second = String(format: "%02d", Int(pickerView.selectedRow(inComponent: 2)))
         
-        timeLabel.text = "\(hour):\(minute):\(second)"
+        timerLabel.text = "\(hour):\(minute):\(second)"
     }
+    
 }
 
